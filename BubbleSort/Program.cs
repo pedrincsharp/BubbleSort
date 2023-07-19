77c8de9e2ceb6791f25ede5871e7e53b37ae0dbf
João Pedro <joopedro@pop-os.localdomain>
2023-07-18 21:14:18 -0300

    Algoritimo finalizado

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿int tam = 0;
+int[] items;
+
+Console.WriteLine("Qual o tamanho do array?");
+tam = int.Parse(Console.ReadLine() ?? throw new Exception("Valor inválido!"));
+items = new int[tam];
+for (int i = 0; i < tam; i++)
+{
+    Console.WriteLine($"Insira o valor N°{i + 1}:");
+    items[i] = int.Parse(Console.ReadLine() ?? throw new Exception("Valor inválido!"));
+}
+
+
+items.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+BubbleSort(ref items, tam);
+items.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+
+
+static void Swap(ref int a, ref int b)
+{
+    (a, b) = (b, a);
+}
+
+static void BubbleSort(ref int[] arrValue, int tam)
+{
+    if (tam < 1) return;
+
+    for (int i = 0; i < tam; i++)
+    {
+        if(tam - 1 < i + 1) break;
+        if(arrValue[i] > arrValue[i + 1])
+            Swap(ref arrValue[i],ref arrValue[i + 1]);
+    }
+
+    BubbleSort(ref arrValue, (tam - 1));
+}
